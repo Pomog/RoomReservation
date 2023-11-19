10bf0a9e791f426de1e9b743d808c4c633934979
Pomog <thoryur@gmail.com>
2023-11-19 18:43:10 +0200

    TestAddDefaultData

---
+++ internal/render/setup_test.go
@@ -0,0 +1,35 @@
+package render
+
+import (
+	"encoding/gob"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+	"udemyCourse1/internal/config"
+	"udemyCourse1/internal/models"
+
+	"github.com/alexedwards/scs/v2"
+)
+
+var session *scs.SessionManager
+var testApp config.AppConfig
+
+func TestMain(m *testing.M) {
+	// data in the session
+	gob.Register(models.Reservation{})
+
+	// change this to true when in production
+	testApp.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = false
+
+	testApp.Session = session
+	app = &testApp
+
+	os.Exit(m.Run())
+}

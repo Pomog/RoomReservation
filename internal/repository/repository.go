cf5d841fd906a516a1728da63a653e924fa32fe3
Pomog <thoryur@gmail.com>
2023-11-27 06:47:54 +0200

    SearchAvailabilityByDatesByRoomID

--- internal/repository/repository.go
+++ internal/repository/repository.go
@@ -1,9 +1,13 @@
 package repository
 
-import "udemyCourse1/internal/models"
+import (
+	"time"
+	"udemyCourse1/internal/models"
+)
 
 type DatabaseRepo interface {
 	AllUsers() bool
 	InserReservation(res models.Reservation) (int, error)
 	InsertRoomRestriction(r models.RoomRestriction) error
+	SearchAvailabilityByDatesByRoomID(start, end time.Time, roomID int) (bool, error)
 }

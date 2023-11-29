10640f1fb49b1c0745ffb3aa430e5fe5fa8cb573
Pomog <thoryur@gmail.com>
2023-11-29 22:03:46 +0200

    make reservation

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -2,7 +2,6 @@ package dbrepo
 
 import (
 	"context"
-	"fmt"
 	"time"
 	"udemyCourse1/internal/models"
 )
@@ -130,13 +129,13 @@ func (m *postgresDBRepo) SearchAvailabilityForAllRooms(start, end time.Time) ([]
 }
 
 // GetRoomById gets a room by ID
-func (m *postgresDBRepo) GetRoomById (id int) (models.Room, error){
+func (m *postgresDBRepo) GetRoomById(id int) (models.Room, error) {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
 	var room models.Room
 
-	query :=`select id, room_name, created_at, updated_at from rooms where id=$1`
+	query := `select id, room_name, created_at, updated_at from rooms where id=$1`
 	row := m.DB.QueryRowContext(ctx, query, id)
 	err := row.Scan(
 		&room.ID,

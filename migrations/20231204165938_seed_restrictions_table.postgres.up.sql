2be36f91ca6759320afb3f1502877eaab053ea7d
Pomog <thoryur@gmail.com>
2023-12-04 19:00:28 +0200

    Adding a migration for seeding restrictions

---
+++ migrations/20231204165938_seed_restrictions_table.postgres.up.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.restrictions (restriction_name,start_date,end_date,created_at,updated_at) VALUES
+	 ('Reservation','2023-11-26','2023-11-27','2023-11-26 00:00:00.000','2023-11-26 00:00:00.000'),
+	 ('Owner Block','2023-12-01','2023-12-03','2023-12-01 00:00:00.000','2023-12-03 00:00:00.000');

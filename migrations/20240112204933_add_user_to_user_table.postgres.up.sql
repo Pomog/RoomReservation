d2ce9eec26ca2443fec308399268c25fe7fa6a04
Pomog <thoryur@gmail.com>
2024-01-12 22:55:23 +0200

    Add admin user to migrations

---
+++ migrations/20240112204933_add_user_to_user_table.postgres.up.sql
@@ -0,0 +1,19 @@
+INSERT INTO
+    users (
+        first_name,
+        last_name,
+        email,
+        password,
+        access_level,
+        created_at,
+        updated_at
+    )
+VALUES (
+        'admin',
+        'admin',
+        'thoryur@gmail.com',
+        '$2a$10$1ILtQrQq3w1cpFSCKeljweQz8B56gb7xDKc4ap68bgqpqVHuURMDy',
+        1,
+        '2023-11-29 18:27:17.551926',
+        '2023-11-29 18:27:17.551926'
+    );
